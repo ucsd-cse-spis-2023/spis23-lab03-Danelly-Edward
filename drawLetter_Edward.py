130a1707d95168121f716f2dacb29a96658f8720
EdwardL33 <141684825+EdwardL33@users.noreply.github.com>
2023-08-14 14:31:25 -0700

    Update drawLetter_Edward.py

--- drawLetter_Edward.py
+++ drawLetter_Edward.py
@@ -2,27 +2,23 @@ def draw_E(the_turtle):
 
   the_turtle.forward(100)
 
-  the_turtle.setpos(0,0)
+  the_turtle.backward(100)
 
   the_turtle.right(90)
 
-  the_turtle.forward(200)
-
-  the_turtle.penup()
-
-  the_turtle.setpos(0,-100)
+  the_turtle.forward(100)
 
   the_turtle.left(90)
 
-  the_turtle.pendown()
-
   the_turtle.forward(50)
 
-  the_turtle.penup()
-  
-  the_turtle.setpos(0,-200)
+  the_turtle.backward(50)
 
-  the_turtle.pendown()
+  the_turtle.right(90)
+
+  the_turtle.forward(100)
+
+  the_turtle.left(90)
 
   the_turtle.forward(100)
 
